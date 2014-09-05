9ecf211c96a53fca66b8a5935c0dc455c14eba8e
orangain <orangain@gmail.com>
2014-09-05 19:34:09 +0900

    Warn when the user doesn't change the ID

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ jQuery(function() {
   jQuery('#dokuwiki__copytonewpage').click(function(e) {
     e.preventDefault();
     var oldId = JSINFO.id;
-    var newId = prompt('Enter new page id', oldId);
-    if (newId !== null) {
-      var url = 'doku.php?id=' + encodeURIComponent(newId) + '&do=edit&copyfrom=' + encodeURIComponent(oldId);
-      location.href = url;
+    while (true) {
+      var newId = prompt("Enter new-page's ID.", oldId);
+      if (newId !== null) {
+        if (newId === oldId) {
+          alert('You must enter a different ID from current page');
+          continue;
+        }
+
+        var url = 'doku.php?id=' + encodeURIComponent(newId) + '&do=edit&copyfrom=' + encodeURIComponent(oldId);
+        location.href = url;
+      }
+      break;
     }
   });
 });
