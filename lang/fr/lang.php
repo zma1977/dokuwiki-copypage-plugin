9d7256800b80a05fcf09b9bb214eb1e95287b85f
Christophe Gragnic <christophegragnic@gmail.com>
2015-10-17 15:07:28 +0200

    French language.

---
+++ lang/fr/lang.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * French language file for copypage plugin
+ *
+ * @author ProfGra <christophegragnic@gmail.com>
+ */
+
+// menu entry for admin plugins
+// $lang['menu'] = 'Your menu entry';
+
+// custom language strings for the plugin
+// $lang['fixme'] = 'FIXME';
+
+$lang['copypage'] = 'Copier cette page';
+
+$lang['js']['enter_page_id'] = "Entrez le nom de la nouvelle page.";
+$lang['js']['different_id_required'] = 'Le nom doit être différent de celui de la page courante.';
