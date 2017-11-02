0dcd64edaf137c221d88b994b52982723c7cd50c
orangain <orangain@gmail.com>
2017-11-02 22:16:55 +0900

    Add spanish translation. Thanks @optiwiper

---
+++ lang/es/lang.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Spanish language file for copypage plugin
+ *
+ * @author optiwiper <adriancapel@gmail.com>
+ */
+
+// menu entry for admin plugins
+// $lang['menu'] = 'Your menu entry';
+
+// custom language strings for the plugin
+// $lang['fixme'] = 'FIXME';
+
+$lang['copypage'] = 'Copia esta página';
+
+$lang['js']['enter_page_id'] = "Introduce el ID de la nueva página.";
+$lang['js']['different_id_required'] = "Debes introducir una ID diferente de la página actual.";
